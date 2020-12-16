73549fb9b55f726a9535e8a7ebb53020d48fac72
tareasR <tareasrojano@gmail.com>
2020-12-15 22:16:53 -0600

    se agrega detección de puerto de despliegue

--- src/main/java/mx/uv/App.java
+++ src/main/java/mx/uv/App.java
@@ -17,11 +17,21 @@ public class App
 {
     public static void main( String[] args )
     {
+        port(getHerokuAssignedPort());
+
         get("/", (req, res) -> {
             Map<String, Object> modelo = new HashMap<>();
             modelo.put("hola", "Mundo de Velocity");
             modelo.put("personita", new Persona("Charles"));
             return new ModelAndView(modelo, "index.vm");
         }, new VelocityTemplateEngine());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
